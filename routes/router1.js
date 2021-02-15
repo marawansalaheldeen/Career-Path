6d9c0fa9dd790c84dcb52ba2ada5404f9d2ecdb7
MS <marawansalaheldeen@gmail.com>
2021-02-16 01:08:10 +0200

    routes

--- routes/router1.js
+++ routes/router1.js
@@ -2,9 +2,34 @@ const express = require('express');
 const router = new express.Router();
 
 const {createNewUser,userLogin}=require('../controller/registration');
+const {
+    getAllTrainingCompany,
+    getAllStudents,
+    getAllDepartments,
+    getAllCourses,
+    getAllCareers,
+    getAllCareerCourse,
+    getAllStudentCourse
+    
+}=require('../controller/getData');
 
 router.post('/newuser',createNewUser);
 router.post('/userlogin',userLogin);
 
+/////////////////////////
+
+router.get('/admin/getAllTrainingCompany',getAllTrainingCompany);
+
+router.get('/admin/getAllStudents',getAllStudents);
+
+router.get('/admin/getAllDepartments',getAllDepartments);
+
+router.get('/admin/getAllCourses',getAllCourses);
+
+router.get('/admin/getAllCareers',getAllCareers);
+
+router.get('/admin/getAllCareerCourse',getAllCareerCourse);
+
+router.get('/admin/getAllStudentCourse',getAllStudentCourse);
 
 module.exports = router;
